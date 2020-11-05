1df07f5badf1b8d4fb277bc0c9036c630cec1fd7
Jedrzej <jedrzej.markowski@sap.com>
2020-11-05 10:42:50 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+import {CdkOverlayOrigin, ConnectedPosition, Overlay} from "@angular/cdk/overlay";
+import {DynamicPageCollapseChangeEvent} from "@fundamental-ngx/platform";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'test-ng10';
+  @ViewChild('overlay')
+  overlay: ElementRef<HTMLElement>;
+
+  fullscreen = false;
+
+  onCollapseChange(event: DynamicPageCollapseChangeEvent): void {
+    console.log('collapse changed');
+  }
+
+  resizeClicked(event: Event): void {
+    event.stopPropagation();
+  }
+
+  openPage(): void {
+    this.fullscreen = true;
+    this.overlay.nativeElement.style.width = '100%';
+  }
+  closePage(event: Event): void {
+    event.stopPropagation();
+    this.fullscreen = false;
+    this.overlay.nativeElement.style.width = '0%';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from "@fundamental-ngx/core";
+import {FormsModule} from "@angular/forms";
+import {OverlayModule} from "@angular/cdk/overlay";
+import {PlatformButtonModule} from "@fundamental-ngx/platform";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    OverlayModule,
+    PlatformButtonModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
